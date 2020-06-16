2986971bd066c7d7f5b0e487b1755a94f8477198
Ivan Svetunkov <ivan@svetunkov.ru>
2020-06-16 14:11:21 +0100

    Tests for the provided values

--- tests/testthat/test_adam.R
+++ tests/testthat/test_adam.R
@@ -206,6 +206,7 @@ test_that("ADAM regression (ALM) on N2568", {
 
 
 #### ETS + ARIMA / ARIMA + ARIMAX ####
+### ETS + ARIMA
 # ETS(ANN) + ARIMA(0,2,2)
 testModel <- adam(Mcomp::M3[[1234]], "ANN", orders=c(0,2,2));
 test_that("ADAM ETS(ANN) + ARIMA(0,2,2) on N1234", {
@@ -236,6 +237,7 @@ test_that("Forecast of ADAM ETS(ZZZ) + SARIMA(2,1,2)(2,1,1)[12] with logS", {
     expect_match(testForecast$side, "upper");
 })
 
+### ARIMA / ARIMAX
 # Pure SARIMA(2,1,2)(2,1,1)[12], Normal
 testModel <- adam(Mcomp::M3[[2568]], "NNN", orders=list(ar=c(2,2),i=c(1,1), ma=c(2,2)), distribution="dlogis");
 test_that("ADAM SARIMA(2,1,2)(2,1,2)[12] with Logistic on N2568", {
@@ -250,19 +252,148 @@ test_that("Forecast of ADAM SARIMA(2,1,2)(2,1,2)[12]", {
 
 # ARIMAX
 testModel <- adam(xreg, "NNN", h=18, orders=list(ar=c(2,0),i=c(1,0), ma=c(2,1)), holdout=TRUE, formula=y~x);
-test_that("ADAM ARIMAX on N2568", {
+test_that("ADAM SARIMAX on N2568", {
     expect_match(testModel$distribution, "dnorm");
 })
 
 # ARIMAX with dynamic xreg
 testModel <- adam(xreg, "NNN", h=18, orders=list(ar=c(2,0),i=c(1,0), ma=c(2,1)), holdout=TRUE, formula=y~x, xregDo="adapt");
-test_that("ADAM ARIMAX with dynamic xreg on N2568", {
+test_that("ADAM SARIMAX with dynamic xreg on N2568", {
     expect_equal(length(testModel$persistence), 14);
 })
 
+#### Provided initial / persistence / phi / arma / B / reuse the model ####
+### Initials
+# ETS(MMM) with provided level
+testModel <- adam(Mcomp::M3[[2568]], "MMM", initial=list(level=5000));
+test_that("ADAM ETS(MMM) with provided level on N2568", {
+    expect_false(testModel$initialEstimated["level"]);
+})
 
-#### Provided initial / persistence / arma ####
+# ETS(MMM) with provided trend
+testModel <- adam(Mcomp::M3[[2568]], "MMM", initial=list(trend=1));
+test_that("ADAM ETS(MMM) with provided trend on N2568", {
+    expect_false(testModel$initialEstimated["trend"]);
+})
 
+# ETS(MMM) with provided seasonal
+testModel <- adam(Mcomp::M3[[2568]], "MMM", initial=list(seasonal=Mcomp::M3[[2568]]$x[1:12]));
+test_that("ADAM ETS(MMM) with provided seasonal components on N2568", {
+    expect_false(testModel$initialEstimated["seasonal"]);
+})
+
+# ETSX(MMN) with provided xreg initials
+testModel <- adam(xreg, "MMN", h=18, holdout=TRUE, formula=y~x,
+                  initial=list(xreg=c(-0.35,-.34,.27,-.46,.07,-0.28,-0.24,0.05,-0.28,-0.34,-0.01)));
+test_that("ADAM ETSX(MMN) with provided xreg initials on N2568", {
+    expect_false(testModel$initialEstimated["xreg"]);
+})
+
+# ETS(ANN) + ARIMA(0,2,2) with provided initials for ARIMA
+testModel <- adam(Mcomp::M3[[1234]], "ANN", orders=c(0,2,2), initial=list(arima=Mcomp::M3[[1234]]$x[1:2]));
+test_that("ADAM ETS(ANN) + ARIMA(0,2,2) with initials for ARIMA on N1234", {
+    expect_false(testModel$initialEstimated["arima"]);
+})
+
+# All provided initials
+testModel <- adam(xreg, "MMM", formula=y~x, orders=c(0,0,2), lags=c(1,12));
+testModel <- adam(xreg, "MMM", formula=y~x, orders=c(0,0,2), lags=c(1,12), initial=testModel$initial);
+test_that("ADAM ETSX(MMM)+ARIMA(0,0,2) with provided initials on N2568", {
+    expect_true(all(!testModel$initialEstimated));
+})
+
+### Persistence
+# ETS(MMM) with provided alpha
+testModel <- adam(Mcomp::M3[[2568]], "MMM", persistence=list(alpha=0.1));
+test_that("ADAM ETS(MMM) with provided alpha on N2568", {
+    expect_equivalent(testModel$persistence["alpha"],0.1);
+})
+
+# ETS(MMM) with provided beta
+testModel <- adam(Mcomp::M3[[2568]], "MMM", persistence=list(beta=0.1));
+test_that("ADAM ETS(MMM) with provided beta on N2568", {
+    expect_equivalent(testModel$persistence["beta"],0.1);
+})
+
+# ETS(MMM) with provided gamma
+testModel <- adam(Mcomp::M3[[2568]], "MMM", persistence=list(gamma=0.1));
+test_that("ADAM ETS(MMM) with provided gamma on N2568", {
+    expect_equivalent(testModel$persistence["gamma"],0.1);
+})
+
+# ETS(MMN) with provided deltas
+testModel <- adam(xreg, "MMN", formula=y~x, persistence=list(delta=0.01), xregDo="adapt");
+test_that("ADAM ETS(MMN) with provided deltas on N2568", {
+    expect_equivalent(testModel$persistence[substr(names(testModel$persistence),1,5)=="delta"],rep(0.01,11));
+})
+
+### Phi
+# ETS(MMdM) with provided phi
+testModel <- adam(Mcomp::M3[[2568]], "MMdM", phi=0.99);
+test_that("ADAM ETS(MMdM) with provided phi on N2568", {
+    expect_equivalent(testModel$phi,0.99);
+})
+
+### arma parameters
+# Provided AR parameters
+testModel <- adam(Mcomp::M3[[2568]], "MMM", orders=c(2,0,2), arma=list(ar=c(0.9,0.3)));
+test_that("ADAM ETS(MMM)+ARIMA(2,0,2) with provided AR on N2568", {
+    expect_equivalent(testModel$arma$ar,c(0.9,0.3));
+})
+
+# Provided MA parameters
+testModel <- adam(Mcomp::M3[[2568]], "MMM", orders=c(2,0,2), arma=list(ma=c(-0.8,-0.4)));
+test_that("ADAM ETS(MMM)+ARIMA(2,0,2) with provided MA on N2568", {
+    expect_equivalent(testModel$arma$ma,c(-0.8,-0.4));
+})
+
+# Provided ARMA parameters
+testModel <- adam(Mcomp::M3[[2568]], "MMM", orders=c(2,0,2), arma=list(ar=c(0.9,0.3), ma=c(-0.8,-0.4)));
+test_that("ADAM ETS(MMM)+ARIMA(2,0,2) with provided ARMA on N2568", {
+    expect_equivalent(testModel$arma$ar,c(0.9,0.3));
+    expect_equivalent(testModel$arma$ma,c(-0.8,-0.4));
+})
+
+### B
+# Provided starting parameters
+testModel <- adam(Mcomp::M3[[2568]], "NNN", orders=c(2,0,2));
+testModel <- adam(Mcomp::M3[[2568]], "NNN", orders=c(2,0,2), B=testModel$B);
+test_that("ADAM ARIMA(2,0,2) with provided B on N2568", {
+    expect_equivalent(testModel$model,"ARIMA(2,0,2)");
+})
+
+### Model reused
+# Reuse ETS
+testModel <- adam(Mcomp::M3[[2568]], "MMdM");
+testModelNew <- adam(Mcomp::M3[[2568]], testModel);
+test_that("Reuse ADAM ETS(MMdM) on N2568", {
+    expect_match(testModel$model,testModelNew$model);
+    expect_equal(nparam(testModelNew),1);
+})
+
+# Reuse ARIMA
+testModel <- adam(Mcomp::M3[[2568]], "NNN", h=18, orders=list(ar=c(2,0),i=c(1,0), ma=c(2,1)), holdout=TRUE);
+testModelNew <- adam(Mcomp::M3[[2568]], testModel);
+test_that("Reuse ADAM SARIMA(2,1,2)(0,0,1)[12] on N2568", {
+    expect_match(testModel$model,testModelNew$model);
+    expect_equal(nparam(testModelNew),1);
+})
+
+# Reuse ARIMAX
+testModel <- adam(xreg, "NNN", h=18, orders=list(ar=c(2,0),i=c(1,0), ma=c(2,1)), holdout=TRUE, formula=y~x, xregDo="adapt");
+testModelNew <- adam(xreg, testModel);
+test_that("Reuse ADAM SARIMAX(2,1,2)(0,0,1)[12] with dynamic xreg on N2568", {
+    expect_equal(testModel$persistence,testModelNew$persistence);
+    expect_equal(nparam(testModelNew),1);
+})
+
+# Reuse ETSX + ARIMA
+testModel <- adam(xreg, "ANN", h=18, orders=list(ar=c(2,0),i=c(1,0), ma=c(2,1)), holdout=TRUE, formula=y~x);
+testModelNew <- adam(xreg, testModel);
+test_that("Reuse ADAM ETSX(ANN)+SARIMA(2,1,2)(0,0,1)[12] on N2568", {
+    expect_equal(testModel$persistence,testModelNew$persistence);
+    expect_equal(nparam(testModelNew),1);
+})
 
 #### auto.adam ####
 
