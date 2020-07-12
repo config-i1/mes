39f9302a88a8fe4ce1b82bc9a13f37dc5cff1f38
Ivan Svetunkov <ivan@svetunkov.ru>
2020-07-12 15:16:12 +0100

    Remove dgnorm from auto.adam - it can be considered redundant here.

--- R/autoadam.R
+++ R/autoadam.R
@@ -11,8 +11,8 @@
 #' @rdname adam
 #' @export
 auto.adam <- function(y, model="ZXZ", lags=c(frequency(y)), orders=list(ar=c(0),i=c(0),ma=c(0),select=FALSE),
-                      distribution=c("dnorm","dlaplace","ds","dgnorm",
-                                     "dlnorm","dllaplace","dls","dlgnorm","dinvgauss"),
+                      distribution=c("dnorm","dlaplace","ds",
+                                     "dlnorm","dllaplace","dls","dinvgauss"),
                       h=0, holdout=FALSE,
                       persistence=NULL, phi=NULL, initial=c("optimal","backcasting"), arma=NULL,
                       occurrence=c("none","auto","fixed","general","odds-ratio","inverse-odds-ratio","direct"),
